1ab339384eb3e375ce25405a53f6df9bd8192b32
lucianoleyriaa <lucho@postcron.com>
2022-03-31 21:22:17 -0300

    Reactive forms exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+   public form!: FormGroup;
+
+   constructor(private fb: FormBuilder) {}
+
+   ngOnInit(): void {
+       this.form = this.fb.group({
+         name: ['', Validators.required],
+         age: ['', [Validators.required, Validators.max(180), this.onlyNumber()]],
+         company: ['', Validators.required],
+         mail: ['', [Validators.required, Validators.email]]
+       });
+   }
+
+   sendForm(): void {
+      console.log(this.form.value);
+   }
+
+   onlyNumber(): ValidatorFn {
+      return (control: AbstractControl): { [key: string]: boolean } | null => {
+         if (!control.value) return null;
+
+         const value = parseInt(control.value);
+         if (isNaN(value)) {
+            return { 'notANumber': true }
+         } else {
+            return null
+         }
+      }
+   }
 }
