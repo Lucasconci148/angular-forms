26f409a5016a44831213333393b6511b6950d7de
Kiyoshi Uehara <kevin.kiyoshi.uehara@gmail.com>
2022-04-07 17:42:24 -0300

    Added formBuilder to app component form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  public userForm: FormGroup;
+  constructor (private form: FormBuilder) {
+    this.userForm = this.form.group({
+      name: ['', Validators.required],
+      age: ['', Validators.compose([Validators.required, Validators.max(180)])],
+      company: ['', Validators.required],
+      email: ['', Validators.compose([Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")])],
+      //used a custom pattern to verify email input since Validators.email has anomalous behaviors
+    })
+  }
+
+  sendForm() {
+    console.log(this.userForm.value)
+  }
 }
